4dfd258fdaabf130bed738ad9fbb9e05d734e669
Alex Sinnott <chess2ryme@gmail.com>
2016-10-08 13:01:23 +1100

    Version 1.2

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const electron = require('electron')
+const {shell} = require('electron');
 // Module to control application life.
 const app = electron.app
+var WebSocket = require('ws')
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -10,21 +12,41 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 1000, height: 600, icon: __dirname + '/scarlet.ico',
+     show: false, frame: false})
+    // and load the index.html of the app.
+    // mainWindow.loadURL(`https://staging.australianarmedforces.org/mods/electron/?noheader`)
+    mainWindow.loadURL(`https://scarlet.australianarmedforces.org/key/electron/`)
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+	var wsUri = "ws://localhost:1001";
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	websocket = new WebSocket(wsUri);
+	websocket.onerror = function(evt) {
+		var executablePath =  __dirname + "/resources/Scarlet/Scarlet.exe";
+		var parameters = [""];
+
+		shell.openItem(executablePath);
+	};
+
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+
+        mainWindow.openDevTools();
+
+    })
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null
+    })
+
+
+
 }
 
 // This method will be called when Electron has finished
@@ -34,19 +56,10 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    app.quit();
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
 })
 
 // In this file you can include the rest of your app's specific main process
